9e3df8c524ee6dfebf1222dbf77de9b04548daec
GavRaz1 <gavindiscord01@gmail.com>
2025-09-12 06:13:09 -0500

    finished init and started drive function on handwritten drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/subsystems/drive/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/subsystems/drive/Drivetrain.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.resources.subsystems.drive;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,7 +22,44 @@ public class Drivetrain extends SubsystemBase {
         leftRear.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
         rightRear.setDirection(DcMotor.Direction.FORWARD);
-        /* encoder directions */
+        /* motor run modes */
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        /* imu mapping+config */
+        imu = hMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot cHubOrientation = new RevHubOrientationOnRobot( //CHANGE THESE VALUES ONCE BOT IS BUILT
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP
+        );
+        imu.initialize(new IMU.Parameters(cHubOrientation));
+    }
+
+    /** Parameters for the power of each drive motor. (updated 9/12/25)
+     *
+     * @param forward The forward power of the given drive motor (backwards if negative)
+     * @param strafe The strafe power of the given drive motor (left <-> right)
+     * @param turn The rotation power of the given drive motor (in radians [probably lol])
+     *
+     */
+    public void drive(double forward, double strafe, double turn) {
+        double[] drivePowers = { //a few magic numbers here refer to the comments (sorry!)
+                forward + strafe + turn, //leftFront
+                forward - strafe + turn, //leftRear
+                forward - strafe - turn, //rightFront
+                forward + strafe - turn  //rightRear
+        };
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0; //NOTE: consider adding speed modes here if desired (not required but would be neat)
+
+        /* drive power mapping */
+        for (int i = 0; i > 4; i++) {
+            maxPower = Math.max(maxPower, Math.abs(i));
+        }
 
     }
 }
