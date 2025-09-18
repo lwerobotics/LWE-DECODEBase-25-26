d9236702ad679373a785f39625b99faef5db19ad
GavRaz1 <gavindiscord01@gmail.com>
2025-09-18 07:04:36 -0500

    pray stick filter makes sense :sob:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/util/FilterStickInput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/util/FilterStickInput.java
@@ -2,10 +2,8 @@ package org.firstinspires.ftc.teamcode.resources.util;
 
 public class FilterStickInput {
     public double filterStickInput(double input) {
-        if () {
-
-        }
-
-        double adjustedInput = ;
+        if (Math.abs(input) < GamepadConstants.STICKDEADZONE.getEnumValue()) { return 0.0; }
+        double adjustedInput = (Math.abs(input) - GamepadConstants.STICKDEADZONE.getEnumValue()) / (1 - GamepadConstants.STICKDEADZONE.getEnumValue());
+        return Math.pow(adjustedInput, GamepadConstants.STICKSENSITIVITY.getEnumValue()) * Math.signum(input);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/util/GamepadConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/util/GamepadConstants.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.teamcode.resources.util;
 
 public enum GamepadConstants {
-    
+    STICKDEADZONE(0.05),
+    STICKSENSITIVITY(2.0);
+
+    private final double value;
+
+    private GamepadConstants(double value) {
+        this.value = value;
+    }
+
+    public double getEnumValue() {
+        return value;
+    }
 }
