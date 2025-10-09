2cb94fa7444dcb4a8580c2c04ab1c3bc5334a79c
GavRaz1 <gavindiscord01@gmail.com>
2025-10-08 22:26:27 -0500

    tweaked outtake code, added a couple extra opmodes for practice n such, started implementation for intake + textwall on practice tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/PracticeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/PracticeTeleOp.java
@@ -3,37 +3,45 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.resources.commands.drive.DriveField;
 import org.firstinspires.ftc.teamcode.resources.commands.drive.InitDrive;
-import org.firstinspires.ftc.teamcode.resources.commands.drive.SetGlobalPowers;
 import org.firstinspires.ftc.teamcode.resources.subsystems.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.resources.subsystems.outtake.Flywheels;
 import org.firstinspires.ftc.teamcode.resources.util.FilterStickInput;
 
-@TeleOp(group = "TeleOp OpModes", name = "Competition TeleOp")
-public class MecanumTeleOp extends CommandOpMode {
-    /* hardware+utilities */
+public class PracticeTeleOp extends CommandOpMode {
+    /* subsystems */
     private Drivetrain drivetrain;
+    private Flywheels outtake;
+    /* utilities */
     private FilterStickInput fsi;
+    /* hardware */
     private GamepadEx driverOp;
     private GamepadEx toolOp;
-    /* drive commands*/
-    private SetGlobalPowers setGlobalPowers;
+    /* primitives */
+    private double power;
 
     @Override
     public void initialize() {
         /* init */
         drivetrain = new Drivetrain();
+        outtake = new Flywheels();
         fsi = new FilterStickInput();
         driverOp = new GamepadEx(gamepad1);
         toolOp = new GamepadEx(gamepad2);
-        /* drive */
+        power = 0.0;
+        /* drive (G1) */
         schedule(new SequentialCommandGroup(
                 new InitDrive(hardwareMap, drivetrain),
                 new DriveField(drivetrain, fsi.filterStickInput(driverOp.getLeftX()), fsi.filterStickInput(driverOp.getLeftY()), fsi.filterStickInput(driverOp.getRightX()))
         ));
-        /* INSERT HEADER HERE */
-        /* NOTE: in the future once the bot is actually built this area would use GamepadEx buttons/triggers attached to instant co*/
+
+        /* NOTICE:
+        *  THIS OPMODE SHALL BE USED BY THE PROGRAMMING TEAM TO MESS AROUND WITH SO THAT WE CAN USE THIS DURING PRACTICE
+        *  BETWEEN COMPS AND THEN ONCE WE HAVE GOOD CODE PUSH THIS TO THE COMPETITION TELEOP (DENOTED IN THE ANNOTATION
+        *  ABOVE THE CLASS) SO THAT WE DONT RUN INTO ANY SHENANIGANS DURING COMP (HAPPENED A BIT LAST YEAR WAS LOWK QUITE
+        *  BUNS)
+        */
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/subsystems/outtake/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/subsystems/outtake/Flywheels.java
@@ -9,11 +9,10 @@ public class Flywheels extends SubsystemBase {
     private MotorEx wheelMotor;
 
     /**
-     * Parameters for the initialization function of the given flywheel motor (updated 10/7/25)
-     *
+     * Parameters for the initialization function of the given flywheel motor (updated 10/8/25)
      * @param hMap The hardware map used to register hardware to the robot (like motors, servos , actuators, etc.)
      * @param name The string name for the given motor ('flywheelL', 'flywheelR', etc.)
-     * @return
+     * @return Returns the initialized motor from the function
      */
     public MotorEx init(HardwareMap hMap, String name) {
         /* motor mapping */
