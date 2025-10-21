f168d4dc0060596e75d1af7bbeebaa7c550a843c
GavRaz1 <gavindiscord01@gmail.com>
2025-10-21 08:54:30 -0500

    microscopic gate code update (lowk commitfarming)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/subsystems/gate/Gate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/subsystems/gate/Gate.java
@@ -4,12 +4,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.hardware.servos.ServoEx;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 public class Gate extends SubsystemBase {
     public ServoEx gateServo;
 
     public void initGate(HardwareMap hMap) {
-        /* servo mapping */
-        gateServo = new ServoEx(hMap, "gateServo");
+        /* servo mapping+range config */
+        gateServo = new ServoEx(hMap, "gateServo", 90.0, AngleUnit.RADIANS); //lowk ask vin or baron about this just to be safe
     }
 
     public void block() {
